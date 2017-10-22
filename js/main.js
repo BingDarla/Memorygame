17f619ec955a7b8bea0920cd05c235e0489935b1
hbshuang <hbshuang@gmail.com>
2017-10-22 15:09:48 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -18,28 +18,43 @@ var cards = [{
 }
 ];
 var cardsInPlay=[];
+var cardIndex=[];
+var check = null;
 
 var flipCard = function ( ){
 	var cardId =this.getAttribute ('data-id');
     console.log ("User flipped " + cards[cardId].rank);
     cardsInPlay.push (cards[cardId].rank);
+    cardIndex.push (cardId);
     console.log (cards[cardId].cardImage);
     console.log (cards[cardId].suit);
     this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length ===2){
     	cardsCheck ();
     	cardsInPlay=[];
+    	if (!check)
+    	{
+    		document.getElementsByTagName('img')[cardIndex[0]].setAttribute('src', 'images/back.png');
+    	    document.getElementsByTagName('img')[cardIndex[1]].setAttribute('src', 'images/back.png');
+    		cardIndex=[];
+    	}
     }
+
 
 }
 
+
+
 var cardsCheck = function(){
-	if (cardsInPlay[0]===cardsInPlay[1])
+	if (cardsInPlay[0]===cardsInPlay[1]){
 		alert ("You win the game !");
+		check = 1;
+	}
 	else
 		{
-			alert ("Sorry, try again please!");
-	       
+		 alert ("Sorry, try again please!");
+		 check = 0;
+		 
 
 };
 }
