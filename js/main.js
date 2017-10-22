a07f5bbab7af72802433197fa4709cbc6997de73
hbshuang <hbshuang@gmail.com>
2017-10-22 15:38:18 +1100

    created game logic"
    exit

--- js/main.js
+++ js/main.js
@@ -18,26 +18,30 @@ var cards = [{
 }
 ];
 var cardsInPlay=[];
-var cardIndex=[];
 var check = null;
+var cardInit = function (){
+	for (var i=0;i<cards.length;i++)
+	{
+		var x=document.getElementsByTagName('img');
+		x[i].setAttribute ('src','images/back.png');
+	}
+}
 
 var flipCard = function ( ){
 	var cardId =this.getAttribute ('data-id');
     console.log ("User flipped " + cards[cardId].rank);
-    cardsInPlay.push (cards[cardId].rank);
-    cardIndex.push (cardId);
+    cardsInPlay.push (cards[cardId]);
     console.log (cards[cardId].cardImage);
     console.log (cards[cardId].suit);
     this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length ===2){
     	cardsCheck ();
-    	cardsInPlay=[];
     	if (!check)
     	{
-    		document.getElementsByTagName('img')[cardIndex[0]].setAttribute('src', 'images/back.png');
-    	    document.getElementsByTagName('img')[cardIndex[1]].setAttribute('src', 'images/back.png');
-    		cardIndex=[];
+
+    		cardInit();
     	}
+    	cardsInPlay=[];
     }
 
 
@@ -46,7 +50,7 @@ var flipCard = function ( ){
 
 
 var cardsCheck = function(){
-	if (cardsInPlay[0]===cardsInPlay[1]){
+	if (cardsInPlay[0].rank===cardsInPlay[1].rank){
 		alert ("You win the game !");
 		check = 1;
 	}
